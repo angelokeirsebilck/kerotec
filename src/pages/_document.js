4b740681bc58a3fdec7a434eca63233baece331f
Angelo Keirsebilck <angelokeirsebilck@Mac-Pro-van-Angelo.local>
2022-04-10 21:57:44 +0200

    Change font import

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="nl">
       <Head />
       <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@100&family=Montserrat:ital,wght@0,300;0,500;0,600;0,700;1,400&family=Oswald:wght@200;300;400;500;600;700&family=Poppins:wght@100&family=Roboto:ital,wght@0,100;1,100;1,300&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100&family=Montserrat:ital,wght@0,300;0,500;0,600;0,700;1,400&display=swap"
         rel="stylesheet"
       ></link>
       <body>
