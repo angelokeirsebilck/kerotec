0c7668600752f4c807aec9e15d4884667527a2a0
Angelo Keirsebilck <angelokeirsebilck@Mac-Pro-van-Angelo.local>
2022-04-11 08:54:55 +0200

    Font optimization test

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,11 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="nl">
-      <Head />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@100&family=Montserrat:ital,wght@0,300;0,500;0,600;0,700;1,400&display=swap"
-        rel="stylesheet"
-      ></link>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100&family=Montserrat:ital,wght@0,300;0,500;0,600;0,700;1,400&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
