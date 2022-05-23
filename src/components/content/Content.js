1091b9526c8192b8aa2672ee17694a71b109c66d
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2022-05-23 18:28:29 +0200

    Test dynamic + landing animation

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,17 +1,18 @@
 import React from "react";
-import Cta from "./CTA";
-import Form from "./Form";
-import References from "./References";
-import Text from "./Text";
-import TextMedia from "./TextMedia";
-import USP from "./USP";
+import dynamic from "next/dynamic";
+// import Cta from "./CTA";
+// import Form from "./Form";
+// import References from "./References";
+// import Text from "./Text";
+// import TextMedia from "./TextMedia";
+// import USP from "./USP";
 
-// const USP = dynamic(() => import("./USP"));
-// const TextMedia = dynamic(() => import("./TextMedia"));
-// const Text = dynamic(() => import("./Text"));
-// const References = dynamic(() => import("./References"));
-// const Form = dynamic(() => import("./Form"));
-// const Cta = dynamic(() => import("./CTA"));
+const USP = dynamic(() => import("./USP"));
+const TextMedia = dynamic(() => import("./TextMedia"));
+const Text = dynamic(() => import("./Text"));
+const References = dynamic(() => import("./References"));
+const Form = dynamic(() => import("./Form"));
+const Cta = dynamic(() => import("./CTA"));
 
 const Content = ({ content, usp, info, lcp }) => {
   return content.map((c, index) => {
