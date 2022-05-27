9bb3e0dbab4084e4ec637baf29a243a48ad92ba9
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2022-05-27 08:31:07 +0200

    test

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -40,69 +40,69 @@ const Services = dynamic(() => import("./Services"));
 // });
 
 const Content = ({ content, usp, info, lcp, services }) => {
-  // const getContentBlock = () => {
-  //   return content.map((c, index) => {
-  //     switch (c.__typename) {
-  //       case "fieldContentKerotec_typeText_BlockType":
-  //         return <Text key={`Text${c.id}`} content={c} />;
-  //       case "fieldContentKerotec_typeTextMedia_BlockType":
-  //         return (
-  //           <TextMedia
-  //             key={`TextMedia${c.id}`}
-  //             content={c}
-  //             lcp={lcp}
-  //             index={index}
-  //           />
-  //         );
-  //       case "fieldContentKerotec_typeUsp_BlockType":
-  //         return <USP key={`USP${c.id}`} content={c} usp={usp} />;
-  //       case "fieldContentKerotec_typeCta_BlockType":
-  //         return <Cta key={`Cta${c.id}`} content={c} />;
-  //       case "fieldContentKerotec_typeForm_BlockType":
-  //         return <Form key={`Form${c.id}`} info={info} content={c} />;
-  //       case "fieldContentKerotec_typeReferences_BlockType":
-  //         return <References key={`References${c.id}`} content={c} />;
-  //       case "fieldContentKerotec_typeServices_BlockType":
-  //         return (
-  //           <Services key={`Services${c.id}`} content={c} services={services} />
-  //         );
-  //       default:
-  //         return null;
-  //     }
-  //   });
-  // };
+  const getContentBlock = () => {
+    return content.map((c, index) => {
+      switch (c.__typename) {
+        case "fieldContentKerotec_typeText_BlockType":
+          return <Text key={`Text${c.id}`} content={c} />;
+        case "fieldContentKerotec_typeTextMedia_BlockType":
+          return (
+            <TextMedia
+              key={`TextMedia${c.id}`}
+              content={c}
+              lcp={lcp}
+              index={index}
+            />
+          );
+        case "fieldContentKerotec_typeUsp_BlockType":
+          return <USP key={`USP${c.id}`} content={c} usp={usp} />;
+        case "fieldContentKerotec_typeCta_BlockType":
+          return <Cta key={`Cta${c.id}`} content={c} />;
+        case "fieldContentKerotec_typeForm_BlockType":
+          return <Form key={`Form${c.id}`} info={info} content={c} />;
+        case "fieldContentKerotec_typeReferences_BlockType":
+          return <References key={`References${c.id}`} content={c} />;
+        case "fieldContentKerotec_typeServices_BlockType":
+          return (
+            <Services key={`Services${c.id}`} content={c} services={services} />
+          );
+        default:
+          return null;
+      }
+    });
+  };
 
-  return content.map((c, index) => {
-    switch (c.__typename) {
-      case "fieldContentKerotec_typeText_BlockType":
-        return <Text key={`Text${c.id}`} content={c} />;
-      case "fieldContentKerotec_typeTextMedia_BlockType":
-        return (
-          <TextMedia
-            key={`TextMedia${c.id}`}
-            content={c}
-            lcp={lcp}
-            index={index}
-          />
-        );
-      case "fieldContentKerotec_typeUsp_BlockType":
-        return <USP key={`USP${c.id}`} content={c} usp={usp} />;
-      case "fieldContentKerotec_typeCta_BlockType":
-        return <Cta key={`Cta${c.id}`} content={c} />;
-      case "fieldContentKerotec_typeForm_BlockType":
-        return <Form key={`Form${c.id}`} info={info} content={c} />;
-      case "fieldContentKerotec_typeReferences_BlockType":
-        return <References key={`References${c.id}`} content={c} />;
-      case "fieldContentKerotec_typeServices_BlockType":
-        return (
-          <Services key={`Services${c.id}`} content={c} services={services} />
-        );
-      default:
-        return null;
-    }
-  });
+  // return content.map((c, index) => {
+  //   switch (c.__typename) {
+  //     case "fieldContentKerotec_typeText_BlockType":
+  //       return <Text key={`Text${c.id}`} content={c} />;
+  //     case "fieldContentKerotec_typeTextMedia_BlockType":
+  //       return (
+  //         <TextMedia
+  //           key={`TextMedia${c.id}`}
+  //           content={c}
+  //           lcp={lcp}
+  //           index={index}
+  //         />
+  //       );
+  //     case "fieldContentKerotec_typeUsp_BlockType":
+  //       return <USP key={`USP${c.id}`} content={c} usp={usp} />;
+  //     case "fieldContentKerotec_typeCta_BlockType":
+  //       return <Cta key={`Cta${c.id}`} content={c} />;
+  //     case "fieldContentKerotec_typeForm_BlockType":
+  //       return <Form key={`Form${c.id}`} info={info} content={c} />;
+  //     case "fieldContentKerotec_typeReferences_BlockType":
+  //       return <References key={`References${c.id}`} content={c} />;
+  //     case "fieldContentKerotec_typeServices_BlockType":
+  //       return (
+  //         <Services key={`Services${c.id}`} content={c} services={services} />
+  //       );
+  //     default:
+  //       return null;
+  //   }
+  // });
 
-  // return <Suspense fallback={<Spinner />}>{getContentBlock()}</Suspense>;
+  return <Suspense fallback={<Spinner />}>{getContentBlock()}</Suspense>;
 };
 
 export default Content;
